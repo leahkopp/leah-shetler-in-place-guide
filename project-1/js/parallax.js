40bd0f595cf600902d189222272b72e1491e856d
leahkopp <63757401+leahkopp@users.noreply.github.com>
2020-06-11 10:05:00 -0700

    Add files via upload

---
+++ project-1/js/parallax.js
@@ -0,0 +1,15 @@
+$(window).scroll(function () {
+    var wScroll = $(this).scrollTop();
+    
+    $(".title").css({
+      transform: "translate(0px, " + wScroll / 2 + "%)"
+    });
+  
+  
+    $(".transparent").css({
+    transform: "translate(0px, -" + wScroll / 3 + "%)"
+    });
+
+});
+  
+  
